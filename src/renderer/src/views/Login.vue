1ac2e6a4546d1131ade2e3f221f5b6bff6bd54ca
iiinsane <kaveriapu116@gmail.com>
2024-06-09 15:01:18 +0800

    null

---
+++ src/renderer/src/views/Login.vue
@@ -0,0 +1,10 @@
+<template>
+  <div></div>
+</template>
+
+<script setup>
+import { ref, reactive, getCurrentInstance, nextTick } from "vue"
+const { proxy } = getCurrentInstance();
+</script>
+
+<style lang="scss" scoped></style>
